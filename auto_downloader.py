5db6b6c39bbb53fa50c490dc0d16b83413e697b0
Tim Ubial <tubial@sd38.bc.ca>
2025-01-17 10:34:29 -0800

    auto downloader changes

--- auto_downloader.py
+++ auto_downloader.py
@@ -10,14 +10,34 @@ import bs4      # beautiful soup - parse HTML info
 url = "https://xkcd.com"       # starting url
 
 for _ in range(1):
-    # TODO: download the page
+    # download the page
     print(f"Downloading page {url}...")
     
     response = requests.get(url)  # grabs the page info
     response.raise_for_status()
-    print(response.text)
 
-    # TODO: find the url of the of the comic image
-    # TODO: download the image
-    # TODO: save the file to a specific folder
+    # find the url of the of the comic image
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+
+    comic_element = soup.select("#comic img")
+
+    if not comic_element:       # if search is empty
+        print("Could not find image.")
+    else:
+        comic_url = "https:" + comic_element[0].get("src")
+
+        # download the image
+        print(f"Downloading {comic_url}...")
+
+        response = requests.get(comic_url)
+        response.raise_for_status()
+
+        # TODO: save the file to a specific folder
+        with open(os.path.join("xkcd_images", os.path.basename(comic_url)), "wb") as image_file:
+            # Fill the file in 100000 byte chunks
+
+            for chunk in response.iter_content(100_000):
+                image_file.write(chunk)
+
+
     # TODO: go to the next image -> getting the prev button's url
